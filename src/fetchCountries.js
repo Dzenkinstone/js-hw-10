2cc80c4bcca1594690b4efb979009f2c30c1553e
Dzenkinstone <kozlovdanil6@gmail.com>
2022-11-20 19:01:24 +0100

    /changes/

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -10,7 +10,7 @@ export const Fetch = class Fetch {
       `https://restcountries.com/v3.1/name/${this.searchQuery}?fields=name,capital,population,flags,languages`
     )
       .then(this.errorResponce)
-      .then(this.dataChange)
+      .then(data => data)
       .catch(error => console.log(error));
   }
 
@@ -19,36 +19,6 @@ export const Fetch = class Fetch {
       Notiflix.Notify.failure('Oops, there is no country with that name');
     }
     return resp.json();
-  }
-
-  dataChange(data) {
-    if (data.length > 10) {
-      return Notiflix.Notify.info(
-        'Too many matches found. Please enter a more specific name.'
-      );
-    } else if (data.length >= 2) {
-      return (refs.div.innerHTML = data
-        .map(elements => {
-          return `<div class="country-container"><img class="country-img" height="20" width="20" src="${elements.flags.svg}"><span class="country-name">${elements.name.official}</span></div>`;
-        })
-        .join(''));
-    }
-
-    return (refs.div.innerHTML = data
-      .map(elements => {
-        return `<div class="js-container"><img class="js-img" height="25" width="25" src="${
-          elements.flags.svg
-        }"><h1 class="js-title">${
-          elements.name.official
-        }</h1><span><strong>Capital:</strong> ${
-          elements.capital
-        }</span><span><strong>Population:</strong> ${
-          elements.population
-        }</span><span><strong>Languages:</strong> ${Object.values(
-          elements.languages
-        )}</span></div>`;
-      })
-      .join(''));
   }
 
   get query() {
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ refs.input.addEventListener(
   debounce(event => {
     if (event.target.value != '') {
       fetch.query = event.target.value.trim();
-      fetch.fetchCountries();
+      fetch.fetchCountries().then(dataChange);
     } else {
       clearContainer();
     }
@@ -26,4 +26,34 @@ refs.input.addEventListener(
 
 function clearContainer() {
   refs.div.innerHTML = '';
+}
+
+function dataChange(data) {
+  if (data.length > 10) {
+    return Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (data.length >= 2) {
+    return (refs.div.innerHTML = data
+      .map(elements => {
+        return `<div class="country-container"><img class="country-img" height="20" width="20" src="${elements.flags.svg}"><span class="country-name">${elements.name.official}</span></div>`;
+      })
+      .join(''));
+  }
+
+  return (refs.div.innerHTML = data
+    .map(elements => {
+      return `<div class="js-container"><img class="js-img" height="25" width="25" src="${
+        elements.flags.svg
+      }"><h1 class="js-title">${
+        elements.name.official
+      }</h1><span><strong>Capital:</strong> ${
+        elements.capital
+      }</span><span><strong>Population:</strong> ${
+        elements.population
+      }</span><span><strong>Languages:</strong> ${Object.values(
+        elements.languages
+      )}</span></div>`;
+    })
+    .join(''));
 }
